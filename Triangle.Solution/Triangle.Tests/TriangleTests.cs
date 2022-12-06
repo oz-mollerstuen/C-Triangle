3998a0619818ae40e838fbde2c6ca8a2500e124b
oz-mollerstuen <mollerstuen.works@gmail.com>
2022-12-06 14:22:16 -0800

    readd

--- Triangle.Solution/Triangle.Tests/TriangleTests.cs
+++ Triangle.Solution/Triangle.Tests/TriangleTests.cs
@@ -6,15 +6,37 @@ namespace Triangle.Tests
   [TestClass]
   public class TriangleTests
   {
+    //NameofMethod_TryingToDo_Expection;
     [TestMethod]
-    public void isEqualTriangle_SidesEqual_True() //NameofMethod_TryingToDo_Expection;
+    public void isEqualTriangle_SidesEqual_True() //test for equal triangle
     {
       Triangle testTriangle = new Triangle();
       Assert.AreEqual(true, testTriangle.isEqualTriangle(1,1,1));
       //testing code will go here
     }
 
-    // [TestMethod]
-    // public void isEqualTriangle_SidesEqual_
+    [TestMethod]
+    public void isIsoTriangle_IsoTriangle_True()  //test for iso triangle
+    {
+      Triangle testTriangle = new Triangle();
+      Assert.AreEqual(true, testTriangle.isIsoTriangle(1,2,2));
+    }
+
+    [TestMethod]
+    public void isScalTriangle_ScalTriangle_True()  //test for scalene triangle
+    {
+      Triangle testTriangle = new Triangle();
+      Assert.AreEqual(true, testTriangle.isScalTriangle(1,2,3));  //numbers are the same as isNotTriangle
+    }
+
+    [TestMethod]
+    public void isNotTriangle_Triangle_False()  //test for scalene triangle
+    {
+      Triangle testTriangle = new Triangle();
+      Assert.AreEqual(true, testTriangle.isNotTriangle(1,2,3));
+    }
+
+  
+
   }
 }
